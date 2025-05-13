3b1af5427c78e7cad49064c69e2ed5887bc8bb90
spb1007 <152322569+spb1007@users.noreply.github.com>
2025-05-13 21:34:04 +0200

    Update Movie.java

--- src/ubu/gii/dass/refactoring/Movie.java
+++ src/ubu/gii/dass/refactoring/Movie.java
@@ -11,28 +11,43 @@ package ubu.gii.dass.refactoring;
  * 
  */
 
+
 public class Movie {
-	public static final int CHILDRENS = 2;
 	public static final int REGULAR = 0;
 	public static final int NEW_RELEASE = 1;
+	public static final int CHILDRENS = 2;
 
 	private String _title;
-	private int _priceCode;
+	private Price _price;
 
 	public Movie(String title, int priceCode) {
 		_title = title;
-		_priceCode = priceCode;
-	}
-
-	public int getPriceCode() {
-		return _priceCode;
-	}
-
-	public void setPriceCode(int arg) {
-		_priceCode = arg;
+		setPriceCode(priceCode);
 	}
 
 	public String getTitle() {
 		return _title;
+	}
+
+	public int getPriceCode() {
+		return _price.getPriceCode();
+	}
+
+	// Establece el comportamiento de precio según el tipo
+	public void setPriceCode(int priceCode) {
+		switch (priceCode) {
+			case REGULAR -> _price = new RegularPrice();
+			case NEW_RELEASE -> _price = new NewReleasePrice();
+			case CHILDRENS -> _price = new ChildrensPrice();
+			default -> throw new IllegalArgumentException("Invalid price code");
+		}
+	}
+
+	public double getCharge(int daysRented) {
+		return _price.getCharge(daysRented);
+	}
+
+	public int getFrequentRenterPoints(int daysRented) {
+		return _price.getFrequentRenterPoints(daysRented);
 	}
 }
